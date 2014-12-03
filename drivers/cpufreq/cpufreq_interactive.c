6cb1684e6303de4e6d6ca1056fccb37b05249f3f
Lianwei Wang <a22439@motorola.com>
2014-12-02 17:20:50 -0800

    cpufreq: interactive: only boost tunable affected cpus
    It is not correct to boost all the cpus when tunable boost
    parameters are changed. It also does not need to boost the
    cpus which is already boosted.
    
    Change-Id: If7fb7aad87d7e463dbecc583dca8911a4e30ec4a
    Signed-off-by: Lianwei Wang <a22439@motorola.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -460,8 +460,8 @@ static void cpufreq_interactive_timer(unsigned long data)
 	tunables->boosted = tunables->boost_val || now < tunables->boostpulse_endtime;
 
 	if (cpu_load >= tunables->go_hispeed_load || tunables->boosted) {
-		if (pcpu->policy->cur < tunables->hispeed_freq &&
-		    cpu_load <= MAX_LOCAL_LOAD) {
+
+		if (pcpu->policy->cur < tunables->hispeed_freq) {
 			new_freq = tunables->hispeed_freq;
 		} else {
 			new_freq = choose_freq(pcpu, loadadjfreq);
